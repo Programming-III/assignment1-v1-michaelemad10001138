cbcf9e59028bf85929698e7b1aa1ad1e826459f6
michaelemad10001138 <8s7d2c8ryb@privaterelay.appleid.com>
2025-11-12 15:21:35 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -3,14 +3,19 @@
 
 #include "Animal.h"
 
-//define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
 
+public:
+    Reptile();
+    Reptile(bool v, string n, int a, bool h);
+    ~Reptile();
 
+    void setIsVenomous(bool v);
+    bool getIsVenomous() const;
 
-
-
-
-
-
+    void display() const override;
+};
 
 #endif
