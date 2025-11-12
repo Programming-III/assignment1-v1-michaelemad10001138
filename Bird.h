5f7917b1d1cba03161bc02ea3e977a38df8fc547
michaelemad10001138 <8s7d2c8ryb@privaterelay.appleid.com>
2025-11-12 15:20:30 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -3,12 +3,19 @@
 
 #include "Animal.h"
 
-//define class bird here 
+class Bird : public Animal {
+private:
+    float wingSpan;
 
+public:
+    Bird();
+    Bird(float w, string n, int a, bool h);
+    ~Bird();
 
+    void setWingSpan(float w);
+    float getWingSpan() const;
 
-
-
-
+    void display() const override;
+};
 
 #endif
