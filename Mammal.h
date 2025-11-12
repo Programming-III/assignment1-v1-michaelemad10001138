984421c400b9059d84341823eeed0d88014518c5
michaelemad10001138 <8s7d2c8ryb@privaterelay.appleid.com>
2025-11-12 15:21:16 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,11 +3,19 @@
 
 #include "Animal.h"
 
-//define mammal class here
+class Mammal : public Animal {
+private:
+    string furColor;
 
+public:
+    Mammal();
+    Mammal(string f, string n, int a, bool h);
+    ~Mammal();
 
+    void setFurColor(string f);
+    string getFurColor() const;
 
-
-
+    void display() const override;
+};
 
 #endif
